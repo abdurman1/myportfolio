0ff7d7da16e8162dde272ff26e76402bf45b3fa8
abdurman1 <97924436+abdurman1@users.noreply.github.com>
2025-05-16 19:21:29 +0000

    May 16, 2025, 1:21 PM

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
           </ul>
         </nav>
       </div>
-      
       <div className='intro'>
-        <h2>Hello!</h2>
+        <h2>Hi I'm a Frontend Developer!</h2>
+        <h1>Abdurrahman Abuoun</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci assumenda veniam cum nemo, mollitia vero magnam explicabo eos tempora vitae saepe nihil, corrupti quas ullam quos blanditiis excepturi perspiciatis ea? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit nam dicta dignissimos. Nostrum nesciunt eius quidem voluptatem laborum, dolores itaque consequuntur suscipit dolorum unde! Id sapiente excepturi sunt alias quas!</p>
       </div>
     </>
