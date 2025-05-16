c60911e8b4dd1ccfd3f80af90bab2a5b74104673
abdurman1 <97924436+abdurman1@users.noreply.github.com>
2025-05-16 20:26:01 +0000

    May 16, 2025, 2:26 PM

--- src/App.jsx
+++ src/App.jsx
@@ -33,8 +33,8 @@ function App() {
           <img src="" alt="" />
         </div>
       </div>
-      <div className='contact'>
-
+      <div className="contact-form">
+        
       </div>
     </>
   )
