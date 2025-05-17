1f7ea5ed29c01fefa792ab773cc17a5f7ed6549c
abdurman1 <97924436+abdurman1@users.noreply.github.com>
2025-05-17 20:17:05 +0000

    May 17, 2025, 2:17 PM

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,13 @@ function App() {
         <div className='toggleBtn'>
           <svg xmlns="http://www.w3.org/2000/svg" width="15" height="100" viewBox="50 50 200 200"><path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
         </div>
+
+        <div className='dropdown'>
+          <ul>
+            <li><a href="#">Github</a></li>
+            <li><a href="#">LinkedIn</a></li>
+        </ul>
+        </div>
       </div>
       <div className='intro'>
         <h2>Hi I'm a <span>Frontend Developer!</span></h2>
