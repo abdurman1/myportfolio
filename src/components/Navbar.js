6709adfb7abeca8858de8f1ffcb9335210646cfc
abdurman1 <97924436+abdurman1@users.noreply.github.com>
2025-05-17 19:59:19 +0000

    May 17, 2025, 1:59 PM

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,10 @@ function Navbar() {
     return (  
         <div className='header'>
             <h1>Abdurrahman Abuoun</h1>
-            <ul>
-                <li><a href="#">Github</a></li>
-                <li><a href="#">LinkedIn</a></li>
-            </ul>
-            <div className='toggleBtn'>
-            <i className='fa-solid fa-bars'></i>
-            </div>
+            <nav>
+                <a href="#">Github</a>
+                <a href="#">LinkedIn</a>
+            </nav>
         </div>
     );
 }
